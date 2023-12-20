38bc136aa6312167362ad94c6988f4d701e4ea03
norihito-yamazaki-frontierfield <n.yamazaki@frontierfield.co.jp>
2023-12-20 18:26:15 +0900

    getAccessTokenのUT書いた

--- test/services/oauth2Service.test.ts
+++ test/services/oauth2Service.test.ts
@@ -10,6 +10,8 @@ describe('IOAuth2Service', () => {
 
         beforeEach(() => {
             mockedAxios = axios as jest.Mocked<typeof axios>;
+            mockedAxios.post.mockClear()
+            mockedAxios.get.mockClear()
         });
 
         describe('getAuthorizationUrl', () => {
@@ -37,23 +39,72 @@ describe('IOAuth2Service', () => {
         })
 
         describe('getAccessToken', () => {
-            it(``, async () => {
+            it(`should retrieve an access token given a valid authorization code and callback URL`, async () => {
                 // Arrange
+                const cognitoDomain = 'https://example.com';
+                const cognitoUserPoolURL = 'https://example-user-pool.com';
+                const clientId = 'test-client-id';
+                const clientSecret = 'test-client-secret';
+                const redirectUri = 'https://callback.com/authorize/callback';
+                const authorizationCode = 'valid-authorization-code';
+
+                const mockResponse = {
+                    data: {
+                        access_token: 'test-access-token',
+                        id_token: 'test-id-token',
+                        refresh_token: 'test-refresh-token',
+                        expires_in: 3600,
+                        token_type: 'Bearer'
+                    }
+                };
+
+                mockedAxios.post.mockResolvedValue(mockResponse);
+                mockedAxios.create.mockReturnThis();
+
+                const loginAuthenticationService: ILoginAuthenticationService =
+                    new CognitoOAuth2Service(cognitoDomain, cognitoUserPoolURL, clientId, clientSecret);
 
                 // Act
+                const actual = await loginAuthenticationService.getAccessToken(authorizationCode, redirectUri);
 
                 // Assert
+                expect(mockedAxios.post).toHaveBeenCalledWith(
+                    `/oauth2/token`,
+                    new URLSearchParams({
+                        grant_type: 'authorization_code',
+                        client_id: 'test-client-id',
+                        redirect_uri: 'https://callback.com/authorize/callback',
+                        code: 'valid-authorization-code'
+                    }),
+                    {
+                        headers: {
+                            'Authorization': 'Basic dGVzdC1jbGllbnQtaWQ6dGVzdC1jbGllbnQtc2VjcmV0',
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    }
+                );
+                expect(actual).toEqual({
+                    id_token: 'test-id-token',
+                    access_token: 'test-access-token',
+                    refresh_token: 'test-refresh-token',
+                    expires_in: 3600,
+                    token_type: 'Bearer'
+                });
             });
 
             it('should propagate the exception to the caller in case of an error', async () => {
                 // Arrange
-                //mockSlackHttpApiClient.post.mockRejectedValue(new Error('Network Error'));
+                mockedAxios.post.mockRejectedValue(new Error('Network Error'));
+                mockedAxios.create.mockReturnThis();
+
+                const loginAuthenticationService: ILoginAuthenticationService =
+                    new CognitoOAuth2Service("", "", "", "");
 
                 // Act&Assert
-                //await expect(postMessageService.postFeedToSlack(rss, channelId, fromName))
-                //.rejects
-                //.toThrow('Network Error');
-                //(mockSlackHttpApiClient.post).toHaveBeenCalledTimes(1);
+                await expect(loginAuthenticationService.getAccessToken("", ""))
+                    .rejects
+                    .toThrow('Network Error');
+                expect(mockedAxios.post).toHaveBeenCalledTimes(1);
             });
         })
 
