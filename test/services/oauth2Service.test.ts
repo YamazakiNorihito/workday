d4ab0fc01224427ad9f545b9c4b754c63b0065bc
norihito-yamazaki-frontierfield <n.yamazaki@frontierfield.co.jp>
2023-12-20 09:29:42 +0900

    cognitoのgetAuthorizationUrlのUT書いた

--- test/services/oauth2Service.test.ts
+++ test/services/oauth2Service.test.ts
@@ -13,31 +13,26 @@ describe('IOAuth2Service', () => {
         });
 
         describe('getAuthorizationUrl', () => {
-            it(`should return the correct authorization URL with necessary query parameters`, async () => {
+            it(`should return the correct authorization URL with necessary query parameters`, () => {
                 // Arrange
                 const cognitoDomain = 'https://example.com';
                 const cognitoUserPoolURL = 'https://example-user-pool.com';
                 const clientId = 'test-client-id';
                 const clientSecret = 'test-client-secret';
                 const callbackUrl = 'https://callback.com/authorize/callback';
-
-                // Act
                 const loginAuthenticationService: ILoginAuthenticationService =
                     new CognitoOAuth2Service(cognitoDomain, cognitoUserPoolURL, clientId, clientSecret);
-                const act = loginAuthenticationService.getAuthorizationUrl(callbackUrl);
+
+                // Act
+                const actual = loginAuthenticationService.getAuthorizationUrl(callbackUrl);
 
                 // Assert
-            });
+                expect(actual.startsWith('https://example.com/oauth2/authorize'));
 
-            it('should propagate the exception to the caller in case of an error', async () => {
-                // Arrange
-                //mockSlackHttpApiClient.post.mockRejectedValue(new Error('Network Error'));
-
-                // Act&Assert
-                //await expect(postMessageService.postFeedToSlack(rss, channelId, fromName))
-                //.rejects
-                //.toThrow('Network Error');
-                //(mockSlackHttpApiClient.post).toHaveBeenCalledTimes(1);
+                const queryParams = new URLSearchParams(actual.split('?')[1]);
+                expect(queryParams.get('response_type')).toBe('code');
+                expect(queryParams.get('client_id')).toBe('test-client-id');
+                expect(queryParams.get('redirect_uri')).toBe('https://callback.com/authorize/callback');
             });
         })
 
