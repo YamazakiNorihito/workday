0ad2f3f3d61fad409bb98154adfc4bb7730f6b8e
norihito-yamazaki-frontierfield <n.yamazaki@frontierfield.co.jp>
2023-12-26 17:28:16 +0900

    IFreeeAuthenticationService refreshTokenのUT実装した

--- tests/services/freeeHrService.test.ts
+++ tests/services/freeeHrService.test.ts
@@ -140,4 +140,89 @@ describe('IFreeeAuthenticationService', () => {
                 .toThrow('公開鍵が公開されいません。');
         });
     })
+
+    describe('refreshToken', () => {
+        it('should retrieve a new set of tokens given a valid refresh token', async () => {
+            // Arrange
+            const clientId = 'test-client-id';
+            const clientSecret = 'test-client-secret';
+            const refreshToken = 'valid-refresh-token';
+
+            const mockResponse = {
+                data: {
+                    access_token: 'test-access-token',
+                    id_token: 'test-id-token',
+                    refresh_token: 'test-refresh-token',
+                    expires_in: 3600,
+                    token_type: 'Bearer',
+                    scope: 'test-scope',
+                    created_at: 1234,
+                    company_id: 5678
+                }
+            };
+            mockedAxios.post.mockResolvedValue(mockResponse);
+
+            const freeeAuthenticationService: IFreeeAuthenticationService =
+                new FreeeAuthenticationService(clientId, clientSecret);
+
+            // Act
+            const actual = await freeeAuthenticationService.refreshToken(refreshToken);
+
+            // Assert
+            expect(actual).toEqual({
+                id_token: 'test-id-token',
+                access_token: 'test-access-token',
+                refresh_token: 'test-refresh-token',
+                expires_in: 3600,
+                token_type: 'Bearer',
+                scope: 'test-scope',
+                created_at: 1234,
+                company_id: 5678
+            });
+        });
+
+        it('should correctly send a request to refresh the token', async () => {
+            // Axiosの実装に依存するため、HttpClientが変わるごとに修正しないといけないUTです。
+            // Arrange
+            const clientId = 'test-client-id';
+            const clientSecret = 'test-client-secret';
+            const refreshToken = 'valid-refresh-token';
+
+            const freeeAuthenticationService: IFreeeAuthenticationService =
+                new FreeeAuthenticationService(clientId, clientSecret);
+
+            // Act
+            const actual = await freeeAuthenticationService.refreshToken(refreshToken);
+
+            // Assert
+            expect(mockedAxios.post).toHaveBeenCalledWith(
+                `/oauth2/token`,
+                new URLSearchParams({
+                    grant_type: 'refresh_token',
+                    client_id: 'test-client-id',
+                    client_secret: 'test-client-secret',
+                    refresh_token: 'valid-refresh-token'
+                }),
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }
+            );
+        });
+
+        it('should propagate the exception to the caller in case of an error', async () => {
+            // Arrange
+            mockedAxios.post.mockRejectedValue(new Error('Network Error'));
+
+            const freeeAuthenticationService: IFreeeAuthenticationService =
+                new FreeeAuthenticationService("", "");
+
+            // Act & Assert
+            await expect(freeeAuthenticationService.refreshToken("invalid-token"))
+                .rejects
+                .toThrow('Network Error');
+            expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+        });
+    })
 })
